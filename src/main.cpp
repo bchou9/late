3c1405bf3fd15b249c70c8b06460a1a3a82235de
bchou9 <bchou9@users.noreply.github.com>
2020-02-15 14:57:36 -0800

    Update main.cpp
    See if you can spot my changes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,149 +1,216 @@
+#include <sfml/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-
 #include <string>
 #include <iostream>
 int main()
 {
-    sf::Vector2i screenDimensions(800, 600);
+	long long int counter = 0;
+	unsigned horizontalTextureOffset = 0;
+	unsigned verticalTextureOffset = 0;
+	float yVel, xAcl, yAcl, levelUpSpeedUp;
+	sf::RenderWindow Window;
+	sf::Texture bTexture;
+	sf::Sprite bImage;
+	sf::Vector2i screenDimensions(800, 600);
+	sf::Vector2f spriteSize = { 54.0f,70.0 };
+	sf::Vector2f spritePosition = { 0.0f,screenDimensions.y - 500 / 2.0f };
+	sf::Vector2f position(screenDimensions.x / 2, screenDimensions.y / 2);
+	sf::RectangleShape rect(sf::Vector2f(20, 20));
+	sf::Texture spriteTexture;
+	sf::IntRect locationInTexture;
+	sf::View view;
 
-    sf::RenderWindow Window;
-    Window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "My First SFML Game");
-    Window.setKeyRepeatEnabled(false);
-    sf::Texture bTexture;
-    sf::Sprite bImage;
+	yVel = xAcl = yAcl = levelUpSpeedUp = 0.0f;
 
-    if(!bTexture.loadFromFile("../resources/Background.jpg"))
-      std::cout << "Could not load background image" << std::endl;
+	Window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "My First SFML Game");
 
-    bImage.setTexture(bTexture);
-    bImage.setScale(9.0f, (float)screenDimensions.y / bTexture.getSize().y);
+	if (!bTexture.loadFromFile("C:\\Users\\delli7desktop\\Documents\\GitHub\\late\\resources\\Background.jpg"))
+		std::cout << "Could not load background image" << std::endl;
 
-    sf::RectangleShape rect(sf::Vector2f(20, 20));
-    rect.setFillColor(sf::Color::Red);
-    rect.setPosition(40, screenDimensions.y-20);
-    sf::Clock clock;
+	bImage.setTexture(bTexture);
+	bImage.setScale(9.0f, (float)screenDimensions.y / bTexture.getSize().y);
+	spriteTexture.loadFromFile("C:\\Users\\delli7desktop\\Documents\\henry\\c++29\\running-sprite_half_size.png");
+	locationInTexture.height = static_cast<int>(spriteSize.y);
+	locationInTexture.width = static_cast<int>(spriteSize.x);
 
-    float moveSpeed = 10000.0f;
-    float xVel = 10000.0f;
-    float yVel = 0.0f;
+	locationInTexture.left = horizontalTextureOffset * spriteSize.x;
+	locationInTexture.top = 0;
 
-    float xAcl = 0.0f;
-    float yAcl = 0.0f;
+	rect.setTexture(&spriteTexture);
+	rect.setPosition(screenDimensions.x, screenDimensions.y);
 
-    sf::View view;
+	view.reset(sf::FloatRect(0, 0, screenDimensions.x, screenDimensions.y));
+	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
 
-    view.reset(sf::FloatRect(0, 0, screenDimensions.x, screenDimensions.y));
-    view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
+	while (Window.isOpen()) {
+		if (counter >= 9000)
+		{
+			//TODO: reset the view to beginning (wrapping the image).
+		}
+		sf::Event Event;
+		while (Window.pollEvent(Event)) {
+			switch (Event.type) {
+			case sf::Event::Closed:  Window.close();
+				break;
+				Window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (Event.key.code == sf::Keyboard::Escape) {
+					Window.close();
+				}
+				if (Event.key.code == sf::Keyboard::Space) {
+					if (rect.getPosition().y == screenDimensions.y - 20)
+						yAcl = 1.00f;
+				}
+				break;
+			}
+		}
 
-    sf::Vector2f position(screenDimensions.x / 2, screenDimensions.y / 2);
+		if (rect.getPosition().y < screenDimensions.y - 20) {
+			yAcl = -0.0003f;
+		}
 
-    while(Window.isOpen()){
-      clock.restart();
-      sf::Event Event;
-      while(Window.pollEvent(Event)){
-        switch(Event.type){
-          case sf::Event::Closed:  Window.close();
-            break;
-            Window.close();
-            break;
-          case sf::Event::KeyPressed:
-            if(Event.key.code == sf::Keyboard::Escape){
+		yVel += yAcl;
+		rect.setPosition(rect.getPosition().x, rect.getPosition().y - yVel);
 
-            
-              Window.close();
-              
-            }
-            if(Event.key.code == sf::Keyboard::Space){
-              if(rect.getPosition().y == screenDimensions.y - 20)
-                yAcl = 0.75f;
-            }
-            break;
-              
+		if (rect.getPosition().y > screenDimensions.y - 20) {
+			rect.setPosition(rect.getPosition().x, screenDimensions.y - 20);
+		}
 
-          
-        }
-      }
-      /*
-      if(yAcl > 2.0f){
-        yAcl = 2.0f;
-      }
-      */
-
-      if(rect.getPosition().y < screenDimensions.y - 20){
-        yAcl = -0.0005f;
-      }
-
-      yVel += yAcl;
-      rect.setPosition(rect.getPosition().x, rect.getPosition().y - yVel);
-      
-      if(rect.getPosition().y > screenDimensions.y - 20){
-        rect.setPosition(rect.getPosition().x, screenDimensions.y - 20);
-      }
-      if(rect.getPosition().y == screenDimensions.y - 20){
-        yAcl = 0.0f;
-        yVel = 0.0f;
-      }
-
-      position.x += .2;
-      rect.setPosition(position.x + 40, rect.getPosition().y);
-      /*
-      //rect.move(0, yVel )
-      yVel -= yAcl;
-      rect.setPosition(rect.getPosition().x, rect.getPosition().y + yVel);
-      /*
-      if(rect.getPosition().y >= 0 ){
-         yVel += yAcl;
-         rect.setPosition(rect.getPosition().x, rect.getPosition().y + yVel);
-      }
-      
-        
-      //rect.position.y = rect.getPosition().y + yVel;
-      
-     
-      if(rect.getPosition().y < screenDimensions.y - 20){
-        yAcl += .5f;
-      }else{
-        yAcl = 0.0f;
-        yVel = 0.0f;
-      }
-      std::cout << "yVel: " << yVel << " yPos: " << rect.getPosition().y << std::endl;
-      */
-      /*
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        rect.move(moveSpeed * clock.getElapsedTime().asSeconds(), 0);
-      else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        rect.move(-moveSpeed * clock.getElapsedTime().asSeconds(), 0);
-     
-      if(rect.getPosition().x + 10 > screenDimensions.x / 2)
-        position.x = rect.getPosition().x + 10;
-      else
-        position.x = screenDimensions.x / 2;
-      */
-      
-      /*
-      position.x = rect.getPosition().x + 10 - (screenDimensions.x / 2);
-      position.y = rect.getPosition().y + 10 - (screenDimensions.y / 2);
-
-      if(position.x < 0)
-        position.x = 0;
-      if(position.y < 0)
-        position.y = 0;
-      */
-      //view.reset(sf::FloatRect(position.x, position.y, screenDimensions.x, screenDimensions.y));
-      //if(rect.getPosition().x + 10 > screenDimensions.x / 2)
-
-      view.setCenter(position);
-      Window.setView(view);
-
-      Window.draw(bImage);
-      Window.draw(rect);
-
-      Window.display();
-      Window.clear();
-    }
-
-   
-
-    return 0;
+		levelUpSpeedUp += 0.000001;//testing: to speed up as level progresses.
+		position.x += levelUpSpeedUp;
+		rect.setPosition(position.x, rect.getPosition().y);
+		view.setCenter(position);
+		Window.setView(view);
+		Window.draw(bImage);
+		Window.draw(rect);
+		Window.display();
+		Window.clear();
+		counter++;
+	}
+	return 0;
 }
+/*********************************
+//Sprite movement test code below (comment top main() to test this)
+int main()
+{
+	const sf::Vector2u mainWindowSize= {1024u,768u};
+	sf::RenderWindow window(sf::VideoMode(mainWindowSize.x,mainWindowSize.y), "SFML Sprite Sheet Demonstration", sf::Style::Close);
+	sf::Event evnt;
+	bool pauseFlag = false;
+	auto loopCounter = 0u;
+	sf::Vector2f spriteSize= {54.0f,70.0};
+	sf::Vector2f spritePosition = {0.0f,mainWindowSize.y/2.0f};
+	float xOffset;
+
+	// Set movement direction
+	enum Direction {Right, Left};
+	Direction direction = Right;
+
+	// Create sprite
+	sf::Texture spriteTexture;
+	spriteTexture.loadFromFile("C:\\Users\\delli7desktop\\Documents\\henry\\c++29\\running-sprite_half_size.png");
+	sf::IntRect locationInTexture;
+	locationInTexture.height = static_cast<int>(spriteSize.y);
+	locationInTexture.width = static_cast<int>(spriteSize.x);
+
+	unsigned horizontalTextureOffset = 0;
+	unsigned verticalTextureOffset = 0 ;
+	++verticalTextureOffset %= 2;
+	locationInTexture.left = horizontalTextureOffset * spriteSize.x;
+	locationInTexture.top = 0;
+
+	sf::Sprite sprite(spriteTexture);
+	sprite.setPosition(spritePosition);
+	sprite.setTextureRect(locationInTexture);
+	sprite.setTexture(spriteTexture);
+
+	// Music
+	sf::Music music;
+	music.openFromFile("C:\\Users\\delli7desktop\\Documents\\henry\\c++29\\GreenOnions.ogg");
+	music.setVolume(40);
+	music.play();
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(evnt))
+		{
+			switch (evnt.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::MouseButtonPressed:
+				spritePosition.x = evnt.mouseButton.x;
+				spritePosition.y = evnt.mouseButton.y;
+				break;
+			case sf::Event::KeyPressed:
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+					window.close();
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))  // pause
+					pauseFlag = !pauseFlag;
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+					direction = Left;
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+					direction = Right;
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+					spritePosition.y -= 20.0f;
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+					spritePosition.y += 20.0f;
+				else
+					break;
+			default:
+				;
+			}
+			if (evnt.type == evnt.Closed)
+			{
+				window.close();
+			}
+		}
+		if (!pauseFlag)
+		{
+		   loopCounter++;
+
+			if (loopCounter%500 == 0)
+			{
+
+				if (direction == Right)
+				{
+					if (spritePosition.x > mainWindowSize.x)
+					{
+						direction = Left;
+					}
+				}
+				else
+				{
+					if (spritePosition.x < 0.0f)
+					{
+						direction = Right;
+					}
+				}
+
+				// Set the sprite position in the window
+				//xOffset = direction == Right ? 20.0f : -20.0f;
+				//spritePosition.x += xOffset;
+				sprite.setPosition(spritePosition);
+
+				// Set the texture offset in the sprite sheet
+				if (loopCounter%800 == 0)
+					horizontalTextureOffset = 0;
+				else
+					horizontalTextureOffset++;
+				locationInTexture.top = direction * spriteSize.y;
+				locationInTexture.left = horizontalTextureOffset * spriteSize.x;
+				sprite.setTextureRect(locationInTexture);
+				sprite.setTexture(spriteTexture);
+			}
+		}
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+}
+*/
